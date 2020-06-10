6e411860f96ea6a579f5447f03fe264247d51a02
boido.boido <coreycapstick72@gmail.com>
2020-06-10 17:35:20 -0400

    added logic variables for confirms.  added if statements to store confirms. cleaned up typos in code

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //Generates random psw
-function pswGener();
+function pswGener(){
 
     //user sets psw length
     let lenSel = document.getElementById("lenSel").value;
 
-    let vlaues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+    let values = "";
+    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    let nums = "1234567890";
+    let specChar = "!@#$%^&*()";
 
     let password = ""
+
+    let wantsUpperCase = confirm("Do you want upper case characters?")
+    let wantsLowerCase = confirm("Do you want lower case characters?")
+    let wantsNums = confirm("Do you want upper numerical characters?")
+    let wantsSpecChar = confirm("Do you want special characters?")
+
+    if (wantsUpperCase === true) {
+        values += upperCase
+    }
+
+    if (wantsLowerCase === true) {
+        values += lowerCase
+    }
+
+    if (wantsNums === true) {
+        values += nums
+    }
+
+    if (wantsSpecChar === true) {
+        values += specChar
+    }
+
+    // loop to select characters to .length of lenSel
+    for (let i = 0; i <= lenSel; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+
+    //display gen'd password
+    document.getElementById("password").value = password;
+}
